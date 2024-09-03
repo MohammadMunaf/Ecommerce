ebb4c46f5dea9cd0df2f30653af634dc47863645
Mohammad Munaf <mohammadmehfooz100@gmail.com>
2024-09-03 15:59:30 +0530

    change url

--- client/src/Url.js
+++ client/src/Url.js
@@ -1,2 +1,3 @@
-export const baseUrl = `https://ecommerceserver-s6ql.onrender.com`;
+// export const baseUrl = `https://ecommerceserver-s6ql.onrender.com`;
+export const baseUrl='https://ecommerce-4lkr.vercel.app';
 //export const baseUrl = `http://localhost:3001`
